7b48eab0332f5c3612f13d66826585f5ac597496
lhy <136678037@qq.com>
2021-04-22 16:21:52 +0800

    update version

--- examples/src/main/java/com/alibaba/alink/ALSExample.java
+++ examples/src/main/java/com/alibaba/alink/ALSExample.java
@@ -1,14 +1,18 @@
 package com.alibaba.alink;
 
+import com.alibaba.alink.common.io.filesystem.BaseFileSystem;
+import com.alibaba.alink.common.io.filesystem.FilePath;
+import com.alibaba.alink.common.io.filesystem.HadoopFileSystem;
 import com.alibaba.alink.operator.batch.BatchOperator;
 import com.alibaba.alink.operator.batch.dataproc.SplitBatchOp;
 import com.alibaba.alink.operator.batch.recommendation.AlsRateRecommBatchOp;
 import com.alibaba.alink.operator.batch.recommendation.AlsTrainBatchOp;
+import com.alibaba.alink.operator.batch.sink.AkSinkBatchOp;
 import com.alibaba.alink.operator.batch.source.CsvSourceBatchOp;
 
 /**
- * Example for ALS.
- */
+ *  * Example for ALS.
+ *   */
 public class ALSExample {
 
     public static void main(String[] args) throws Exception {
@@ -33,6 +37,13 @@ public class ALSExample {
         AlsRateRecommBatchOp predictor = new AlsRateRecommBatchOp()
             .setUserCol("userid").setItemCol("movieid").setRecommCol("prediction_result");
         BatchOperator preditionResult = predictor.linkFrom(model, testData).select("rating, prediction_result");
-        preditionResult.print();
+
+        BaseFileSystem<?> hadoopFileSystem = new HadoopFileSystem("3.2.2", "hdfs://10.10.12.141:9000");
+        AkSinkBatchOp akSinkToHdfs = new AkSinkBatchOp()
+                .setFilePath(new FilePath("/tmp/alinkre1", hadoopFileSystem))
+                .setOverwriteSink(true);
+        preditionResult.link(akSinkToHdfs);
+        BatchOperator.execute();
     }
 }
+
